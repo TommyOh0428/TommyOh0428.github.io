347f1b4468b53810122e0841530769eeb642ffb4
Tommy Oh <koa18@sfu.ca>
2026-02-09 17:53:24 -0800

    update the experience section

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -1,9 +1,12 @@
-export const experienceContent = `[SUCCESS] October 2025 - Present: Project Lead @ SFU AKCSE
+export const experienceContent = `[SUCCESS] May 2026 - Present: Cloud DevOps Engineer Co-op @ Samsung Electronics Canada
+  - will be updating soon
+
+[SUCCESS] October 2025 - Present: Project Lead @ SFU AKCSE
   - Leading the engineering team to develop the collaboration project
   - Planning the project roadmap and managing task assignments.
   - Coordinating with stakeholders for requirements gathering and feedback.
 
-[SUCCESS] May 2024 - Present: President @ SFU OS Dev Club
+[SUCCESS] April 2024 - April 2026: President @ SFU OS Dev Club
   - Led the Open Source teams as the president of the SFU OS Development Club.
   - Drived 3+ active projects from proposal to deployment.
   - Organized the events and workshops for club members.
